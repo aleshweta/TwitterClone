1dc067edeed6d0f5c12bd426517890a3e60e581e
Shweta Ale <aleshweta7@gmail.com>
2021-09-27 12:44:17 -0400

    twitter

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -16,16 +16,27 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+
+    override func viewDidAppear(_ animated: Bool) {
+         //once page shows what to appear
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true{
+            self.performSegue(withIdentifier: "LoginToHome", sender: self)
+        }
+    }
+    
+    
     @IBAction func onLoginButton(_ sender: Any) {
         let myUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: myUrl,//what url to call and
                                        success: {
+                                        //don't ask user to login again and again
+                                        UserDefaults.standard.set(true, forKey:"userLoggedIn" ) //name of the value
             self.performSegue(withIdentifier: "LoginToHome", // what to call when sucees
                               sender: self)
                                        }, failure: {(Error) in
                                         print("Couldn't login!")
                                         }) //and waht to call when failure
-        
+       
     }
     
 
